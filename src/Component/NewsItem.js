6272d54be56d5e3d6c68846774565ce09d363025
Deepak Yadav <deepak5282786@gmail.com>
2022-09-15 23:06:24 +0530

    added navbar and card features and fetching data from local sysrem

---
+++ src/Component/NewsItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div className="">
+        <div
+          className="card mb-4"
+          style={{ width: "18rem", marginLeft: "15px" }}
+        >
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
