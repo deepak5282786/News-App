8e339f2eb1397cb7c96b5c352ab7a450ec1542c7
Deepak Yadav <deepak5282786@gmail.com>
2022-09-16 06:52:05 +0530

    Fetching News Categories wise Using React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import React, { Component } from "react";
 import NavBar from "./Component/NavBar";
 import News from "./Component/News";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <News pageSize={5} country="us" category="science" />
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <News keys="" pageSize={5} country="us" category="general" />
+            </Route>
+            <Route exact path="/business">
+              <News keys="" pageSize={5} country="us" category="business" />
+            </Route>
+            <Route exact path="/entertainment">
+              <News
+                keys=""
+                pageSize={5}
+                country="us"
+                category="entertainment"
+              />
+            </Route>
+            <Route exact path="/general">
+              <News
+                keys="general"
+                pageSize={5}
+                country="us"
+                category="general"
+              />
+            </Route>
+            <Route exact path="/health">
+              <News keys="health" pageSize={5} country="us" category="health" />
+            </Route>
+            <Route exact path="/science">
+              <News
+                keys="science"
+                pageSize={5}
+                country="us"
+                category="science"
+              />
+            </Route>
+            <Route exact path="/sports">
+              <News keys="sports" pageSize={5} country="us" category="sports" />
+            </Route>
+            <Route exact path="/technology">
+              <News
+                keys="technology"
+                pageSize={5}
+                country="us"
+                category="technology"
+              />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class NavBar extends Component {
   render() {
@@ -6,7 +7,7 @@ export class NavBar extends Component {
       <div className="bg-info mb-2">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
           <div className="container-fluid">
-            <a className="navbar-brand">SachTak News </a>
+            <Link className="navbar-brand">SachTak News </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -24,49 +25,44 @@ export class NavBar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
+                  <Link className="nav-link active" aria-current="page" to="/">
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    About
-                  </a>{" "}
+                  <Link className="nav-link" to="/business">
+                    Business
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    business
-                  </a>
+                  <Link className="nav-link" to="/entertainment">
+                    Entertainment
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    entertainment
-                  </a>
+                  <Link className="nav-link" to="/general">
+                    General
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    general
-                  </a>
+                  <Link className="nav-link" to="/health">
+                    Health
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    health
-                  </a>
+                  <Link className="nav-link" to="/science">
+                    Science
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    science
-                  </a>
+                  <Link className="nav-link" to="/sports">
+                    Sports
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    sports
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    technology
-                  </a>
+                  <Link className="nav-link" to="/technology">
+                    Technology
+                  </Link>
                 </li>
               </ul>
             </div>
