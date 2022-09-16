9e46440b5dc050943d9cf69fffafca602cf5cd41
Deepak Yadav <deepak5282786@gmail.com>
2022-09-17 04:37:31 +0530

    Adding top Bar and fixing issue with badge in mobile view

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React, { Component } from "react";
 import NavBar from "./Component/NavBar";
 import News from "./Component/News";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 export default class App extends Component {
   pageSize = 15;
+  state = {
+    progress: 0,
+  };
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
   render() {
     return (
       <div>
         <Router>
           <NavBar />
+          <LoadingBar
+            height={3}
+            color="#f11946"
+            progress={this.state.progress}
+          />
           <Switch>
             <Route exact path="/">
               <News
+                setProgress={this.setProgress}
                 key="/general"
                 pageSize={this.pageSize}
                 country="in"
@@ -21,6 +34,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/business">
               <News
+                setProgress={this.setProgress}
                 key="/business"
                 pageSize={this.pageSize}
                 country="in"
@@ -29,6 +43,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/entertainment">
               <News
+                setProgress={this.setProgress}
                 key="entertainment"
                 pageSize={this.pageSize}
                 country="in"
@@ -37,6 +52,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/general">
               <News
+                setProgress={this.setProgress}
                 key="general"
                 pageSize={this.pageSize}
                 country="in"
@@ -45,6 +61,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/health">
               <News
+                setProgress={this.setProgress}
                 key="health"
                 pageSize={this.pageSize}
                 country="in"
@@ -53,6 +70,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/science">
               <News
+                setProgress={this.setProgress}
                 key="science"
                 pageSize={this.pageSize}
                 country="in"
@@ -61,6 +79,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/sports">
               <News
+                setProgress={this.setProgress}
                 key="sports"
                 pageSize={this.pageSize}
                 country="in"
@@ -69,6 +88,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/technology">
               <News
+                setProgress={this.setProgress}
                 key="technology"
                 pageSize={this.pageSize}
                 country="in"
--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -2,17 +2,21 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl, date, author, source } =
+    let { title, description, imageUrl, newsUrl, author, date, source } =
       this.props;
     return (
       <div className="my-3">
         <div className="card">
-          <span
-            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
-            style={{ left: "90%", zIndex: "1" }}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
           >
-            {source}
-          </span>
+            <span className="badge rounded-pill bg-danger"> {source} </span>
+          </div>
           <img
             src={
               !imageUrl
@@ -23,11 +27,12 @@ export class NewsItem extends Component {
             alt="..."
           />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title} </h5>
             <p className="card-text">{description}</p>
             <p className="card-text">
               <small className="text-muted">
-                By {author} on {new Date(date).toGMTString()}
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
               </small>
             </p>
             <a
