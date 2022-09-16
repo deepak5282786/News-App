dac5c821b87faa84ca543ad42918cdf8450d44ee
Deepak Yadav <deepak5282786@gmail.com>
2022-09-16 14:36:49 +0530

    Refactoring News Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import News from "./Component/News";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
+  pageSize = 15;
   render() {
     return (
       <div>
@@ -13,7 +14,7 @@ export default class App extends Component {
             <Route exact path="/">
               <News
                 key="/general"
-                pageSize={6}
+                pageSize={this.pageSize}
                 country="in"
                 category="general"
               />
@@ -21,7 +22,7 @@ export default class App extends Component {
             <Route exact path="/business">
               <News
                 key="/business"
-                pageSize={6}
+                pageSize={this.pageSize}
                 country="in"
                 category="business"
               />
@@ -29,7 +30,7 @@ export default class App extends Component {
             <Route exact path="/entertainment">
               <News
                 key="entertainment"
-                pageSize={6}
+                pageSize={this.pageSize}
                 country="in"
                 category="entertainment"
               />
@@ -37,29 +38,39 @@ export default class App extends Component {
             <Route exact path="/general">
               <News
                 key="general"
-                pageSize={6}
+                pageSize={this.pageSize}
                 country="in"
                 category="general"
               />
             </Route>
             <Route exact path="/health">
-              <News key="health" pageSize={6} country="in" category="health" />
+              <News
+                key="health"
+                pageSize={this.pageSize}
+                country="in"
+                category="health"
+              />
             </Route>
             <Route exact path="/science">
               <News
                 key="science"
-                pageSize={6}
+                pageSize={this.pageSize}
                 country="in"
                 category="science"
               />
             </Route>
             <Route exact path="/sports">
-              <News key="sports" pageSize={6} country="in" category="sports" />
+              <News
+                key="sports"
+                pageSize={this.pageSize}
+                country="in"
+                category="sports"
+              />
             </Route>
             <Route exact path="/technology">
               <News
                 key="technology"
-                pageSize={6}
+                pageSize={this.pageSize}
                 country="in"
                 category="technology"
               />
--- src/Component/News.js
+++ src/Component/News.js
@@ -9,78 +9,56 @@ export class News extends Component {
     pageSize: 8,
     category: "general",
   };
+
   static propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string,
   };
-  constructor() {
-    super();
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1,
     };
+    document.title = `${this.capitalizeFirstLetter(this.props.category)}`;
   }
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6b169ff99816446ca617e9dadc9bf8d9&page=1&pageSize=${this.props.pageSize}`;
+
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6b169ff99816446ca617e9dadc9bf8d9&page={this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
-    let parseData = await data.json();
+    let parsedData = await data.json();
     this.setState({
-      articles: parseData.articles,
-      totalResults: parseData.totalResults,
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
       loading: false,
     });
+  }
+  async componentDidMount() {
+    this.updateNews();
   }
 
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${
-      this.props.country
-    }&category=${
-      this.props.category
-    }&apiKey=6b169ff99816446ca617e9dadc9bf8d9&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parseData = await data.json();
-    this.setState({
-      articles: parseData.articles,
-      page: this.state.page - 1,
-      loading: false,
-    });
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
   };
 
   handleNextClick = async () => {
-    if (
-      this.state.page + 1 >
-      Math.ceil(this.state.totalResults / this.props.pageSize)
-    ) {
-    } else {
-      let url = `https://newsapi.org/v2/top-headlines?country=${
-        this.props.country
-      }&category=${
-        this.props.category
-      }&apiKey=6b169ff99816446ca617e9dadc9bf8d9&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parseData = await data.json();
-
-      this.setState({
-        articles: parseData.articles,
-        page: this.state.page + 1,
-        loading: false,
-      });
-    }
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
   };
+
   render() {
     return (
       <div className="container my-3">
         <h1 className="text-center" style={{ margin: "35px 0px" }}>
           SachTak News - Top HeadLines
+          {this.capitalizeFirstLetter(this.props.category)}
         </h1>
         {this.state.loading && <Spinner />}
         <div className="row">
@@ -93,8 +71,8 @@ export class News extends Component {
                     description={element.description ? element.description : ""}
                     imageUrl={element.urlToImage}
                     newsUrl={element.url}
+                    author={element.author}
                     date={element.publishedAt}
-                    author={element.author ? element.author : "Unknown"}
                     source={element.source.name}
                   />
                 </div>
@@ -108,6 +86,7 @@ export class News extends Component {
             className="btn btn-dark"
             onClick={this.handlePrevClick}
           >
+            {" "}
             &larr; Previous
           </button>
           <button
